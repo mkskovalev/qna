7e2e153dd18eea03b643be4589b00c1bbb6a1914
mkskovalev <maxonemore@ya.ru>
2022-09-30 18:25:58 +0700

    add question & answers models with tests

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
+
+  describe 'associations' do
+    it { should have_many(:answers) }
+  end
+end
