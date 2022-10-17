dddb31ad0fe12ca419b1620a4c5d2f45abb5cf72
mkskovalev <maxonemore@ya.ru>
2022-10-17 22:55:45 +0700

    add url validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
   validates :name, :url, presence: true
+  validates :url, format: { with: /https:\/\/gist.github.com\/[A-Za-z0-9][A-Za-z0-9-]*\/[a-f\d]+/,
+                            message: 'must be a link to a gist.' }
 end
