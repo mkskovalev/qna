9330b4177a99dfc67facbabc09d8b84e5ff79f18
mkskovalev <maxonemore@ya.ru>
2022-12-12 22:10:42 +0700

    fix: add serializers & rewrote cancan exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exeption|
     respond_to do |format|
-      format.json { head :forbidden, content_type: 'application/json' }
       format.html { redirect_to root_path, alert: exeption.message }
     end
   end
