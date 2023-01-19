4564bda493a43ffcbfdbfe8e4486e25f033d5f01
mkskovalev <maxonemore@ya.ru>
2023-01-19 18:41:46 +0700

    rewrite command to do symlink to env

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ namespace :config do
   desc "Symlink application config files."
   task :env_symlink do
     on roles(:app) do
-      execute "ln -s {#{shared_path},#{release_path}/config/app_environment_variables.rb"
+      execute "ln -s #{shared_path}/config/app_environment_variables.rb #{release_path}/config/app_environment_variables.rb"
     end
   end
 end
