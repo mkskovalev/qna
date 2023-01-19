4861b1104ab1193fd3e77f694000427e9616f9ea
mkskovalev <maxonemore@ya.ru>
2023-01-20 01:22:29 +0700

    add some one more settings for smtp

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,5 @@ require_relative 'application'
 app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
 load(app_environment_variables) if File.exists?(app_environment_variables)
 
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.smtp_settings = {
-    domain: 'localhost'
-}
-
 # Initialize the Rails application.
 Rails.application.initialize!
