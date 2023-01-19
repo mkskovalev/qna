f970293a71bd873ef125565ed7e3fd30535ec19a
mkskovalev <maxonemore@ya.ru>
2023-01-20 01:05:04 +0700

    add mailer settings to environment.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # YANDEX
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '89.223.70.71' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :tls => true,
